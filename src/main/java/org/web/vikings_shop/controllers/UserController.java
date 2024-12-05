7015b44c4071080b1f49c50cee5149cee10cd64c
Hemant Dhungana <dhunganahemant07@gmail.com>
2024-12-05 10:03:31 +0545

    Category added

--- src/main/java/org/web/vikings_shop/controllers/UserController.java
+++ src/main/java/org/web/vikings_shop/controllers/UserController.java
@@ -5,13 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.web.vikings_shop.entities.Category;
 import org.web.vikings_shop.entities.Product;
+import org.web.vikings_shop.entities.User;
 import org.web.vikings_shop.repo.CategoryRepo;
+import org.web.vikings_shop.repo.UserRepo;
 import org.web.vikings_shop.service.CategoryService;
 import org.web.vikings_shop.service.ProductService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
@@ -25,6 +29,9 @@ public class UserController {
     @Autowired
     private CategoryRepo categoryRepo;
 
+    @Autowired
+    private UserRepo userRepo;
+
     @RequestMapping("/home")
     public String userHome(Model model){
      List<Product> products =   productService.getProducts();
@@ -32,17 +39,25 @@ public class UserController {
         return "user/userhome";
     }
 
-    @RequestMapping("/category")
-    public String userCategory(Model model){
-      String  id = "646747bhd-e99b-12d0-a356-363543d";
+    @RequestMapping("/category/{id}")
+    public String userCategory(@PathVariable String id,Model model){
       Category category = categoryRepo.getById(id);
        List<Product> products = productService.getProductsByCategory(category);
+       String name = category.getName();
+       model.addAttribute("Cid", id);
+       model.addAttribute("categoryName", name);
+        model.addAttribute("products", products);
 
-       for(Product product : products){
-           System.out.println(product);
+        return "/user/usercategory";
+    }
 
+    @RequestMapping("/profile")
+    public String profile(@RequestParam String email, Model model){
 
-       } return "/user/userhome";
+      Optional<User> user= userRepo.findByEmail(email);
+
+      model.addAttribute("user", user);
+        return "user/userprofile";
     }
 
 
--- src/main/java/org/web/vikings_shop/repo/UserRepo.java
+++ src/main/java/org/web/vikings_shop/repo/UserRepo.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepo extends JpaRepository<User, String> {
    Optional<User> findByEmail(String email);
 
+
 }
